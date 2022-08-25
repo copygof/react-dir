94a50408fa123f6b78121eab963b836df4ae91a3
preecha.s <precha.sattabut@gmail.com>
2022-08-26 01:21:28 +0700

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
 
-function App() {
+
+const PageWithName = ({ name }) => (
+  <main style={{ padding: "1rem 0" }}>
+    <h2>{name}</h2>
+  </main>
+)
+
+const Layout = ({ children }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav style={{
+        borderBottom: "solid 1px",
+        padding: "1rem",
+
+      }}>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/page-a">Page A</Link> |{" "}
+        <Link to="/page-b">Page B</Link>
+      </nav>
+      <Outlet />
     </div>
+  )
+}
+
+function RootApp() {
+  return (
+    <BrowserRouter basename="/iframe">
+      <Routes>
+        <Route path="/" element={<Layout name="Root" />}>
+          <Route index element={<PageWithName name="Page Home" />} />
+          <Route path="page-a" element={<PageWithName name="Page A" />} />
+          <Route path="page-b" element={<PageWithName name="Page b" />} />
+        </Route>
+        <Route path="404" element={<PageWithName name="404" />} />
+        <Route path="*" element={<Navigate to="/404" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App;
+export default RootApp;
